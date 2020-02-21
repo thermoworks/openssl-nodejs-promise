8ecefb8d191e2ea63c4e47c1ffc0bf5c82641a0b
Peter O'Reilly <oreilp25@gmail.com>
2020-02-21 11:16:39 +0000

    promisify and add configuration options to openssl directory

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
-# openssl-nodejs
+# openssl-nodejs-promise
 
-is a package which gives you a possibility to run every [OpenSSL](https://www.openssl.org/) command in [Node.js](https://nodejs.org/en/) in a handy way. Moreover, parameters like -in, -keyin, -config and etc can be replaced by a raw data ([Buffor](https://nodejs.org/dist/latest-v10.x/docs/api/buffer.html)).
+Fork of the very excellent [openssl-nodejs](https://github.com/codevibess/openssl-nodejs)
+`openssl-node-js-promise` is a package which gives you a possibility to run every [OpenSSL](https://www.openssl.org/) command in [Node.js](https://nodejs.org/en/) in a handy way. Moreover, parameters like -in, -keyin, -config and etc can be replaced by a raw data ([Buffor](https://nodejs.org/dist/latest-v10.x/docs/api/buffer.html)).
 
 # Installation &amp; Usage
 
 ```javascript
-npm install openssl-nodejs
+npm install openssl-nodejs-promise
 ```
 
 Import openssl module:
 ```javascript
 const openssl = require('openssl-nodejs')
+openssl(cmd, options).then((res) => ...)
+```
+
+Supported Options
+=================
+The only supported option is the directory `openssl-nodejs-promise` will write to.
+the default is `openssl` in the current directory.
+
+```javascript
+const options = { dir: '/tmp/openssl' }
 ```
 
 Next, invoke openssl function and put command with parameters inside a function like presented in the example below.
 ```javascript
 openssl('openssl req -config csr.cnf -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -keyout key.key -out certificate.crt')
+    .then((buffer) => buffer.toString())
+    .catch(e => console.error(e.stack))
 ```
+
 To get access to the result of execution specify callback function as the last parameter of openssl function (with arguments err and buffer).
 
 openssl function can be invoked with a single parameter like OpenSSL command (see example above) or within an array with command name and parameters itself.
 ```javascript
-openssl(['req', '-config', 'csr.conf', '-out', 'CSR.csr', '-new', '-newkey', 'rsa:2048', '-nodes', '-keyout', 'privateKey.key', function (err, buffer) {
-console.log(err.toString(), buffer.toString());
-});
+openssl(['req', '-config', 'csr.conf', '-out', 'CSR.csr', '-new', '-newkey', 'rsa:2048', '-nodes', '-keyout', 'privateKey.key');
 ```
 
 If you want to specify Buffer text instead of the file as an input/output or whatever you need, use the version with an array as a function parameter.
@@ -35,9 +47,7 @@ Example of object:
 ```
 Command example:
 ```javascript
-openssl(['req', '-config', { name:'csr.conf', buffer: BufferVariable }, '-out', 'CSR.csr', '-new', '-newkey', 'rsa:2048', '-nodes', '-keyout', 'privateKey.key'], function (err, buffer) {
-console.log(err.toString(), buffer.toString());
-});
+openssl(['req', '-config', { name:'csr.conf', buffer: BufferVariable }, '-out', 'CSR.csr', '-new', '-newkey', 'rsa:2048', '-nodes', '-keyout', 'privateKey.key']);
 ```
 
 When you used a command which generates additional output in file format this package will create a folder openssl/ in the directory where the command was invoked. All output files will appear in this folder (openssl).
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@
 /* eslint-disable no-console */
 /* eslint indent: "error" */
 const fs = require('fs');
+const path = require('path');
 const { spawn } = require('child_process');
 
-
-const isFunction = callback => callback instanceof Function
 
 const checkIsParamsString = params => typeof params === 'string'
 
@@ -23,69 +22,70 @@ const checkDataTypeCompatibility = (params) => {
 }
 
 
-module.exports = function openssl(params, callback = () => undefined) {
-    const stdout = [];
-    const stderr = [];
-    const dir = 'openssl/';
-    let parameters = params
+module.exports = function openssl(params, options = { dir: '' }) {
+    return new Promise((resolve, reject) => {
+        const stdout = [];
+        const stderr = [];
+        const dir = path.resolve(options.dir, 'openssl');
+        let parameters = params
+
+        if (!checkDataTypeCompatibility(parameters)) {
+            return reject(new Error(`Parameters  must be string or an array, but got ${typeof parameters}`));
+        }
+
+        if (checkIsParamsString(parameters)) {
+            parameters = parameters.split(' ')
+        }
+
+        if (!checkIfParamsArrayIsEmpty(parameters)) {
+            return reject(new Error('Array of params must contain at least one parameter'));
+        }
+
+        if (parameters[0] === 'openssl') parameters.shift()
 
 
-    if (!isFunction(callback)) {
-        throw new Error(`Callback must be a function, but got a ${typeof callback}`)
-    }
+        for (let i = 0; i <= parameters.length - 1; i++) {
+            
+            if (checkBufferObject(parameters[i])) {
+                if (!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir);
+                }
 
-    if (!checkDataTypeCompatibility(parameters)) {
-        throw new Error(`Parameters  must be string or an array, but got ${typeof parameters}`);
-    }
+                const filename = path.resolve(dir, parameters[i].name);
 
-    if (checkIsParamsString(parameters)) {
-        parameters = parameters.split(' ')
-    }
+                fs.writeFileSync(filename, parameters[i].buffer, (err) => {
+                    if (err) {
+                        console.log(err);
+                        return reject(new Error(err));
+                    }
+                });
 
-    if (!checkIfParamsArrayIsEmpty(parameters)) throw new Error('Array of params must contain at least one parameter');
-
-    if (parameters[0] === 'openssl') parameters.shift()
-
-
-    for (let i = 0; i <= parameters.length - 1; i++) {
-        
-        if (checkBufferObject(parameters[i])) {
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir);
+                parameters[i] = parameters[i].name
+                parameters[i] = path.resolve(dir, parameters[i]);            
             }
 
-            const filename = dir + parameters[i].name
-
-            fs.writeFileSync(filename, parameters[i].buffer, (err) => {
-                if (err) {
-                    throw new Error(err);
-                }
-            });
-
-            parameters[i] = parameters[i].name
-            parameters[i] = dir + parameters[i];            
+            if (checkCommandForIO(parameters[i]) && typeof parameters[i + 1] !== 'object') {
+                parameters[i + 1] = dir + parameters[i + 1];
+            }
         }
 
-        if (checkCommandForIO(parameters[i]) && typeof parameters[i + 1] !== 'object') {
-            parameters[i + 1] = dir + parameters[i + 1];
-        }
-    }
 
+        const openSSLProcess = spawn('openssl', parameters);
 
-    const openSSLProcess = spawn('openssl', parameters);
+        openSSLProcess.stdout.on('data', (data) => {
+            stdout.push(data);
+        });
 
-    openSSLProcess.stdout.on('data', (data) => {
-        stdout.push(data);
-    });
+        openSSLProcess.stderr.on('data', (data) => {
+            stderr.push(data)
+        });
 
-    openSSLProcess.stderr.on('data', (data) => {
-        stderr.push(data)
-    });
+        openSSLProcess.on('close', (code) => {
+            if (stderr.length > 0) {
+                return reject(new Error(Buffer.concat(stderr)).toString())
+            }
+            return resolve(Buffer.concat(stdout));
+        });
 
-    openSSLProcess.on('close', (code) => {
-        console.log(`OpenSSL process ends with code ${code}`)
-        callback.call(null, stderr, stdout);
-    });
-
-    return openSSLProcess
+    })
 }
